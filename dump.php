e3ebe84970cc75af89d9bbd0a4ccabed9d165dba
Ananta Raihan <anantaraihan19@gmail.com>
2026-07-19 12:40:55 +0700

    chore: remove temp scripts

--- dump.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require_once 'bootstrap/app.php';
-$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-$page = App\Models\Page::where('slug', 'profil-bandara-kalimarau')->first();
-echo $page ? $page->content : 'Not found';
